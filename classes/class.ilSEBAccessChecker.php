411cf88e3f624c35b5a5f0628ba52206e051954e
Stephan Kergomard <webmaster@kergomard.ch>
2022-10-26 10:46:44 +0300

    Fix Missing Default Value

--- classes/class.ilSEBAccessChecker.php
+++ classes/class.ilSEBAccessChecker.php
@@ -37,19 +37,19 @@ class ilSEBAccessChecker
     private $data;
     private $ref_id;
     private $mode;
-    
+
     private $current_user_allowed;
     public function isCurrentUserAllowed() : bool
     {
         return $this->current_user_allowed;
     }
-    
+
     private $switch_to_seb_skin_needed;
     public function isSwitchToSebSkinNeeded() : bool
     {
         return $this->switch_to_seb_skin_needed;
     }
-    
+
     /**
      * We set everying up in the constructor, populating all variables
      *
@@ -70,49 +70,49 @@ class ilSEBAccessChecker
         $this->rbacreview = $rbacreview;
         $this->http = $http;
         $this->conf = $conf;
-        
+
         $this->mode = $this->sebKeyInHeaderPostCookieOrNone();
         $this->data = $this->retrieveSEBData($this->mode);
         $this->ref_id = $ref_id;
-        
+
         $is_logged_in = ($this->user->id && $this->user->id != ANONYMOUS_USER_ID);
         $is_root = $this->rbacreview->isAssigned($this->user->id, SYSTEM_ROLE_ID);
         $this->switch_to_seb_skin_needed = $this->detectSwitchToSEBSkinNeeded($is_logged_in, $is_root);
         $this->current_user_allowed = $this->detectCurrentUserAllowed($is_logged_in, $is_root);
-        
+
         ilSession::set('last_uri', $this->retrieveFullUri());
     }
-    
+
     public function isKeyCheckPossibleOrUnavoidable() : bool
     {
         if ($this->mode === ilSEBPlugin::SEB_DATA_MODE['none'] && $this->data['uri'] === '' && $this->data['last_uri'] !== '') {
             return false;
         }
-        
+
         return true;
     }
-    
+
     public function exitIlias(ilSEBPlugin $pl) : void
     {
         $response = $this->http->response();
         $request = $this->http->request();
-        
+
         ilSession::setClosingContext(ilSession::SESSION_CLOSE_LOGIN);
         if ($this->auth->isValid()) {
             $this->auth->logout();
         }
         session_unset();
         session_destroy();
-        
+
         $target_str = '';
         if ($this->ref_id) {
             $target_str = 'target=' . \ilObject::_lookupType($this->ref_id, true) . '_' . $this->ref_id . '&';
         } elseif ($target = $request->getQueryParams()['target']) {
             $target_str = 'target=' . $target . '&';
         }
-        
+
         $response = $response->withStatus(403, "Forbidden");
-            
+
         $tpl = $pl->getTemplate('default/tpl.seb_forbidden.html');
         $tpl->setCurrentBlock('seb_forbidden_message');
         $tpl->setVariable('SEB_FORBIDDEN_HEADER', $pl->txt('forbidden_header'));
@@ -125,7 +125,7 @@ class ilSEBAccessChecker
         $this->http->sendResponse();
         exit;
     }
-    
+
     private function sebKeyInHeaderPostCookieOrNone() : int
     {
         if ($this->http->request()->hasHeader(ilSEBPlugin::REQ_HEADER)) {
@@ -136,31 +136,31 @@ class ilSEBAccessChecker
         }
         return ilSEBPlugin::SEB_DATA_MODE['none'];
     }
-    
+
     private function detectCurrentUserAllowed(bool $is_logged_in, bool $is_root) : bool
     {
         $role_deny = $this->conf->getRoleDeny();
         $allow_without_seb = true;
-        
+
         if ($is_logged_in && $role_deny && !$is_root) {
             $allow_without_seb = !($role_deny === 1 || $this->rbacreview->isAssigned($this->user->id, $role_deny));
         }
-        
+
         if ($allow_without_seb ||
             ($this->detectSeb($this->ref_id) >= ilSEBPlugin::SEB_REQUEST_TYPES['seb_request']) ||
             ($this->anySEBKeyIsEnough() && $this->detectSeb())) {
             return true;
         }
-        
+
         return false;
     }
-    
+
     private function anySEBKeyIsEnough() : bool
     {
         $cmd = $this->ctrl->getCmd();
         $cmdclass = $this->ctrl->getCmdClass();
         $path = $this->http->request()->getUri()->getPath();
-        
+
         if (in_array(ilContext::getType(), ilSEBPlugin::REQUESTS_THAT_DONT_NEED_OBJECT_SPECIFIC_KEYS['context_check']) ||
             in_array($cmd, ilSEBPlugin::REQUESTS_THAT_DONT_NEED_OBJECT_SPECIFIC_KEYS['cmd_check']) ||
             (array_key_exists($cmdclass, ilSEBPlugin::REQUESTS_THAT_DONT_NEED_OBJECT_SPECIFIC_KEYS['cmd_and_cmdclass_check']) &&
@@ -168,53 +168,53 @@ class ilSEBAccessChecker
             ) {
             return true;
         }
-        
+
         foreach (ilSEBPlugin::REQUESTS_THAT_DONT_NEED_OBJECT_SPECIFIC_KEYS['path_check'] as $exempted_path) {
             if (mb_strpos($path, $exempted_path)) {
                 return true;
             }
         }
-        
+
         return false;
     }
-    
+
     private function detectSwitchToSEBSkinNeeded(bool $is_logged_in, bool $is_root) : bool
     {
         $is_kiosk_user = (($this->conf->getRoleKiosk() == 1 || $this->rbacreview->isAssigned($this->user->id, $this->conf->getRoleKiosk())) && !$is_root);
-        
+
         if ($is_logged_in && $is_kiosk_user) {
             return true;
         }
-        
+
         return false;
     }
-    
-    private function detectSeb(?int $ref_id) : int
+
+    private function detectSeb(?int $ref_id = null) : int
     {
         $exam_key = $this->data['exam_key'];
-        
+
         if ($exam_key === "") {
             return ilSebPlugin::SEB_REQUEST_TYPES['not_a_seb_request'];
         }
-        
+
         if ($this->conf->checkSebKey($exam_key, $this->data['uri'])) {
             return ilSebPlugin::SEB_REQUEST_TYPES['seb_request'];
         }
         if ($this->conf->checkObjectKey($exam_key, $this->data['uri'], $ref_id)) {
             return ilSebPlugin::SEB_REQUEST_TYPES['seb_request_object_keys'];
         }
-        
+
         if (!$ref_id && $this->conf->checkKeyAgainstAllObjectKeys($exam_key, $this->data['uri'])) {
             return ilSebPlugin::SEB_REQUEST_TYPES['seb_request_object_keys_unspecific'];
         }
-        
+
         return ilSebPlugin::SEB_REQUEST_TYPES['seb_request_invalid'];
     }
-    
+
     private function retrieveSEBData(int $mode) : array
     {
         $data = [];
-        
+
         switch ($mode) {
             case ilSEBPlugin::SEB_DATA_MODE['header']:
                 $data['exam_key'] = $this->http->request()->getHeader(ilSEBPlugin::REQ_HEADER)[0];
@@ -229,11 +229,11 @@ class ilSEBAccessChecker
                 $data['uri'] = isset($_COOKIE['uri']) ? $_COOKIE['uri'] : '';
                 break;
         }
-        
+
         $data['last_uri'] = ilSession::get('last_uri');
         return $data;
     }
-    
+
     private function retrieveFullUri() : string
     {
         $uri = $this->http->request()->getUri();
@@ -242,12 +242,12 @@ class ilSEBAccessChecker
         $host = $uri->getHost();
         $path = $uri->getPath();
         $query = $uri->getQuery();
-        
+
         if ($query != '') {
             $query = '?' . $query;
         }
-        
-        
+
+
         return $protocol . "://" . $host . $port . $path . $query;
     }
 }
